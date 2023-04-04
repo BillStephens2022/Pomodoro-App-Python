7b3b6309a432c92ec94cdcdac722f434fa8798fb
BillStephens2022 <two4onebill@yahoo.com>
2023-04-04 12:38:22 -0400

    got timer to countdown seconds

--- main.py
+++ main.py
@@ -14,7 +14,16 @@ LONG_BREAK_MIN = 20
 # TODO:
 # Timer Reset
 # Timer Mechanism
+
+def start_timer():
+    countdown(WORK_MIN * 60)
+
 # Countdown Mechanism
+def countdown(count):
+    if count > 0:
+        canvas.itemconfig(timer_text, text=count)
+        window.after(1000, countdown, count - 1)
+
 
 # UI Setup
 window = Tk()
@@ -24,13 +33,13 @@ window.config(padx=100, pady=50, bg=YELLOW) #refers to YELLOW constant variable 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0) #highlightthickness removes the border around the image
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 timer_label.grid(column=1, row=0)
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0)
