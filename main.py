ea223a775ae6ef297ec1951d2c748f6b27f23b7a
BillStephens2022 <two4onebill@yahoo.com>
2023-04-04 11:36:39 -0400

    set up GUI with image, text, background colors

--- main.py
+++ main.py
@@ -15,4 +15,16 @@ LONG_BREAK_MIN = 20
 # Timer Reset
 # Timer Mechanism
 # Countdown Mechanism
+
 # UI Setup
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW) #refers to YELLOW constant variable defined above
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0) #highlightthickness removes the border around the image
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+window.mainloop()
